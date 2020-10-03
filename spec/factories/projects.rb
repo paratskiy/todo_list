1cb621cb7a51537405c07e814aee9db481054ef1
Bogdan Paratskiy <bog4242@gmail.com>
2020-10-03 18:14:02 +0300

    add spec for project model

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :project do
-    name { "MyString" }
-    user { nil }
+    name { FFaker::Lorem.sentence }
+    user
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:project) { FactoryBot.create :project }
+
+  def build_custom_entity(subject, **params)
+    FactoryBot.build(subject, params)
+  end
+
+  context 'correct project' do
+    it 'should be valid' do
+      expect(project).to be_valid
+    end
+  end
+
+  context 'with empty data' do
+    it 'user id should be present' do
+      expect(build_custom_entity(:project, user_id: nil)).not_to be_valid
+    end
+
+    it 'name should be present' do
+      project.name = ' '
+      expect(project).not_to be_valid
+    end
+  end
 end
