b41e204cc875765502ebfd0d1bd03a5a38648f2a
Bogdan Paratskiy <bog4242@gmail.com>
2020-10-03 16:38:59 +0300

    add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 end
