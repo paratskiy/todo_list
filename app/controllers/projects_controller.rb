40925d97396f384dd68c3183600fb34dd68184c8
Bogdan Paratskiy <bog4242@gmail.com>
2020-10-06 21:57:36 +0300

    finish projects with ajax

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
+  before_action :set_project, only: %i[edit update destroy]
+  after_action :set_errors, only: %i[create update]
 
   def index
     @projects = current_user.projects
@@ -7,41 +9,63 @@ class ProjectsController < ApplicationController
 
   def new
     @project = current_user.projects.new
+    respond_to do |format|
+      format.html { redirect_to :root }
+      format.js
+    end
   end
 
   def create
     @project = current_user.projects.build(project_params)
-    if @project.save
-      flash[:success] = 'Project created!'
-      redirect_to :root
-    else
-      render 'new'
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to :root, notice: 'Project was created.' }
+        format.json { render :root, status: :created, location: @project }
+      else
+        format.html { redirect_to :root }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def edit
-    @project = Project.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to :root }
+      format.js
+    end
   end
 
   def update
-    @project = Project.find(params[:id])
-    if @project.update(project_params)
-      flash[:success] = 'Project updated!'
-      redirect_to :root
-    else
-      render 'edit'
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to :root, notice: 'Project was updated.' }
+        format.json { render :root, status: :ok, location: @project }
+      else
+        format.html { redirect_to :root }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
-    Project.find(params[:id]).destroy
-    flash[:success] = 'Project deleted!'
-    redirect_to :root
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to :root, notice: 'Project was destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
 
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
   def project_params
     params.require(:project).permit(:name)
+  end
+
+  def set_errors
+    session[:errors] = @project.errors.full_messages
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
+  after_action :clear_errors, only: %i[home]
+
   def home
     return unless user_signed_in?
 
     @projects = current_user.projects
+  end
+
+  private
+
+  def clear_errors
+    session[:errors] = nil
   end
 end
